66fd8cd23d50e4cc6eb3ffb5a718f3ae1a94b404
IPSITA ROY <ipshitaroy2007@gmail.com>
2026-04-26 01:23:47 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(bodyParser.json());
 app.use(express.static('../frontend/public')); // Serve frontend
 
